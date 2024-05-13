0a2faf04eb3810ea1d7c75a82c8b2c88ec2f50a6
Lavanya <lavanyaramesh319@gmail.com>
2024-05-13 11:07:38 +0530

    password variable added

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_ssm_parameter" "parameters" {
   key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type  = "SecureString"
+  key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
+  overwrite = true
+}
