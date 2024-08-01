9d982589fa640af80f7f23349405c3a4f0d7b983
lavanya <lavanyaramesh319@gmail.com>
2024-08-01 22:15:32 +0530

    updation of terraform ansible

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_ssm_parameter" "parameters" {
-  count = length(var.parameters)
-  name  = var.parameters[count.index].name
-  value = var.parameters[count.index].value
-  type  = "String"
-  key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
-  overwrite = true
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
 
+## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
+## One of the scenario that mostly used in real way is , Enter the passwords manually in Parameter Store by someone for passwords.
 resource "aws_ssm_parameter" "passwords" {
-  count = length(var.passwords)
-  name  = var.passwords[count.index].name
-  value = var.passwords[count.index].value
-  type  = "SecureString"
-  key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
-  overwrite = true
+  count  = length(var.passwords)
+  name   = var.passwords[count.index].name
+  value  = var.passwords[count.index].value
+  type   = "SecureString"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
