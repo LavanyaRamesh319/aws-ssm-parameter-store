95ef412cc79dfd3462c3cdfe44ba774c6670859f
lavanya <lavanyaramesh319@gmail.com>
2024-08-02 10:53:17 +0530

    corrected the error

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
 }
 
 ## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,5 +13,5 @@ resource "aws_ssm_parameter" "passwords" {
   name   = var.passwords[count.index].name
   value  = var.passwords[count.index].value
   type   = "SecureString"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "bc770a11-96d3-4f3a-8ce2-2856e8e6c0ca"
 }
